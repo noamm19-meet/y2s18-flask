62ab335022c5106b3ff52e91ffc6eb6fc782091b
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 18:31:32 +0300

    remember to forkgit add .!

--- exercises/app.py
+++ exercises/app.py
@@ -2,8 +2,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+def regester_page():
+    return render_template(
+    	"hello1.html")
+
+@app.route('/home')
 def home_page():
-    pass
+    return render_template(
+    	"hello.html")
 
 if __name__ == '__main__':
    app.run(debug = True)
