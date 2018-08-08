726fc11bfe0455556d8a9db3c2e48ba91e4803af
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 10:31:05 +0300

    flask

--- exercises/app.py
+++ exercises/app.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def regester_page():
-    return render_template(
-    	"hello1.html")
+#@app.route('/')
+#def regester_page():
+#    exersice=["pullup" , " pushup" , "musculeup" , "situp" , "planche" ]
+#    return render_template(
+#        "hello1.html",
+#        exersice=exersice,
+#        likes_same_sport=True)
 
 @app.route('/home')
 def home_page():
     return render_template(
-    	"hello.html")
+       "hello.html")
 
 if __name__ == '__main__':
    app.run(debug = True)
